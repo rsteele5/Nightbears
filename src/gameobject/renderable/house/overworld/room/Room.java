46e823988701a0f76779f021acfb86b97f79a4dc
Remington <remington.steele312@gmail.com>
2019-03-14 07:41:43 -0500

    this too. see commit before

--- src/gameobject/renderable/house/overworld/room/Room.java
+++ src/gameobject/renderable/house/overworld/room/Room.java
@@ -1,7 +1,9 @@
-package gameobject.renderable.house.overworld;
+package gameobject.renderable.house.overworld.room;
 
 import gameobject.renderable.RenderableObject;
 import gameobject.renderable.DrawLayer;
+import main.utilities.Debug;
+import main.utilities.DebugEnabler;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -11,22 +13,59 @@ public abstract class Room {
     //private ArrayList<Boundary> boundaries;
     //private ArrayList<interactables> boundaries;
     //private ArrayList<Boundary> boundaries;
+    protected String name;
     private int cellX;
     private int cellY;
     private Integer[][] layout;
     //TODO: Setup Anchor points here
 
-    public Room(){
+    public Room(String name){
         layout = constructLayout();
         cellX = -1;
         cellY = -1;
+        this.name = "Room: "+name;
     }
 
     protected abstract Integer[][] constructLayout();
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCellX() {
+        return cellX;
+    }
+
+    public int getCellY() {
+        return cellY;
+    }
+
+    public Integer[][] getLayout() {
+        return layout;
+    }
 
     public void setCell(int x, int y) {
         cellX = x;
         cellY = y;
     }
 
+    public boolean isConflicting(Room newRoom) {
+        if(cellX >= 0 && cellY >= 0){
+            if(newRoom.cellX >= 0 && newRoom.cellY >= 0){
+                if((newRoom.cellX >= cellX && newRoom.cellY >= cellY)
+                        && (newRoom.cellY < cellY + layout.length && newRoom.cellX < cellX + layout[0].length)) {
+                    //TODO: Check if the conflicting sections have irrelevant differences
+                    return true;
+                }
+            }else{
+                Debug.warning(DebugEnabler.OVERWORLD, newRoom.name
+                        + " - is being compared, but does not have a set cell yet. Returning false. ");
+            }
+        }else{
+            Debug.warning(DebugEnabler.OVERWORLD, name
+                    + " - is trying to compare layouts, but does not have a set cell yet. Returning false. ");
+        }
+
+        return false;
+    }
 }
