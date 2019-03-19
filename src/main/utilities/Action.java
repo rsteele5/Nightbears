4f5e9f94e8ccbe2de7ce7d03888bd179bb6bdb13
Remington <remington.steele312@gmail.com>
2019-03-19 15:36:39 -0500

    Added JavaDocs
    Utilities done.

--- src/main/utilities/Action.java
+++ src/main/utilities/Action.java
@@ -1,5 +1,11 @@
 package main.utilities;
 
+/**
+ * Action is an interface that utilizes a single function doIt() that is paramaterless and returns nothing.
+ */
 public interface Action {
+    /**
+     * Returns nothing and accepts nothing. Truly an interface that lets you JUST DO IT!
+     */
     void doIt();
 }
--- src/main/utilities/Clickable.java
+++ src/main/utilities/Clickable.java
@@ -1,7 +1,29 @@
 package main.utilities;
 
+/**
+ * Contains the necessary functions needed to be a clickable object in the game.
+ */
 public interface Clickable {
+    /**
+     * Executes when the object is clicked
+     */
     void onClick();
+
+    /**
+     * Sets the desired functionality of the clickable when it is clicked. The method Accepts and Action, but a
+     * parameterless lamba expression can be passed instead.
+     *
+     * @param   onClick A function dictating how the clickable will behave on click.
+     * @see     Action
+     */
     void setOnClick(Action onClick);
+
+    /**
+     * A check to see if the coordinates passed are contained within the object.
+     * @param x the x coordinate.
+     * @param y the y coordinate.
+     * @return  true: the coordinate is contained in the object. <br>
+     *          false: the coordinate is not contained in the object.
+     */
     boolean contains(int x, int y);
 }
