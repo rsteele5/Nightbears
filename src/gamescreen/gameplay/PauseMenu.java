23d34b59baa6fe6802c473666761e0cb6496f26c
Mari Miller <redreaper33@gmail.com>
2019-04-02 21:34:22 -0500

    Big Inventory

--- src/gamescreen/gameplay/PauseMenu.java
+++ src/gamescreen/gameplay/PauseMenu.java
@@ -22,8 +22,7 @@ import main.utilities.Clickable;
 import main.utilities.Debug;
 import main.utilities.DebugEnabler;
 
-import java.awt.Font;
-import java.awt.Color;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -42,6 +41,26 @@ public class PauseMenu extends Overlay {
     private ButtonText useButton;
     private RenderableGridContainer playerGrid;
     private PlayerData playerData = gameData.getPlayerData();
+    //Numbers
+    int lblYVal = 150;
+    int itemBtnSize = 70;
+    int itemBigSize = itemBtnSize*2;
+    //Define Points
+    Point screenSize = (new Point(1600,900));
+    Point bgLblSpot = (new Point(0,0));
+    Point titleLblSpot = (new Point(5,0));
+    Point itemLblSpot = (new Point(100,lblYVal));
+    Point eqpLblSpot = (new Point(itemLblSpot.x+275,lblYVal));
+    Point selectedLblSpot = (new Point(eqpLblSpot.x+350,lblYVal));
+    Point teddyImgSpot  = (new Point(eqpLblSpot.x+75,560));
+    Point teddyImgSize = (new Point(112,225));
+    Point btnSpot = (new Point(1240,60));
+    Point btnSize = (new Point(340,144));
+    Point itemTxtSpot = (new Point(selectedLblSpot.x+50,selectedLblSpot.y+100));
+    Point btnUseSpot = (new Point(itemTxtSpot.x+45,itemTxtSpot.y+310));
+    Point btnUseSize = (new Point(210,98));
+    Point btnBigSpot = (new Point(btnUseSpot.x+35,btnUseSpot.y+105));
+    Point itemBtnSpot = (new Point(20,250));
     public enum Equipments{
         Helmet,
         OffHand,
@@ -54,7 +73,7 @@ public class PauseMenu extends Overlay {
     //endregion
 
     public PauseMenu(ScreenManager screenManager, GameScreen parentScreen) {
-        super(screenManager, parentScreen, "PauseMenu", 450, 180, 0f);
+        super(screenManager, parentScreen, "PauseMenu", 150, 80, 0f);
         isExclusive = true;
     }
 
@@ -164,7 +183,6 @@ public class PauseMenu extends Overlay {
         currentItemButton.deSelect();
     }
 
-
     private void resetButtonItems(){
         // Reset all player item button to null, set item button again, and establish click events
 
@@ -188,47 +206,45 @@ public class PauseMenu extends Overlay {
 
         //BackGround
         ImageContainer background;
-        background = new ImageContainer(0,0,"/assets/backgrounds/BG-Inventory.png", DrawLayer.Background);
-        background.setWidth(980);
-        background.setHeight(540);
+        background = new ImageContainer(bgLblSpot.x,bgLblSpot.y, "/assets/backgrounds/BG-Inventory.png", DrawLayer.Background);
+        background.setSize(screenSize.x,screenSize.y);
         background.addToScreen(this,true);
 
         //Fonts
-        Font noScaryHeader1 = new Font("NoScary", Font.PLAIN, 96);
-        Font noScaryHeader2 = new Font("NoScary", Font.PLAIN, 60);
+        Font noScaryHeader1 = new Font("NoScary", Font.PLAIN, 192);
+        Font noScaryHeader2 = new Font("NoScary", Font.PLAIN, 100);
 
         //Add menu Title
-        TextBox menuTitle = new TextBox(5, 0, 400, 1, "Pause Menu",
+        TextBox menuTitle = new TextBox(titleLblSpot.x, titleLblSpot.y, 800, 1, "Pause Menu",
                 noScaryHeader1, Color.WHITE);
         menuTitle.addToScreen(this,true);
 
         //Item label
-        TextBox items = new TextBox(75, 75, 250, 100, "Items",
+        TextBox items = new TextBox(itemLblSpot.x, itemLblSpot.y, 500, 200, "Items",
                 noScaryHeader2, Color.BLUE);
         items.addToScreen(this,true);
 
         //Equipped label
-        TextBox equipped = new TextBox(260, 75, 250, 100, "Equipped",
+        TextBox equipped = new TextBox(eqpLblSpot.x, eqpLblSpot.y, 500, 200, "Equipped",
                 noScaryHeader2, Color.BLUE);
         equipped.addToScreen(this,true);
 
         //Selected item label
-        TextBox selectedItem = new TextBox(450, 75, 250, 100, "Selected Item",
+        TextBox selectedItem = new TextBox(selectedLblSpot.x, selectedLblSpot.y, 800, 200, "Selected Item",
                 noScaryHeader2, Color.BLUE);
         selectedItem.addToScreen(this,true);
 
         //Teddy
-        ImageContainer teddy = new ImageContainer(295,370,
+        ImageContainer teddy = new ImageContainer(teddyImgSpot.x,teddyImgSpot.y,
                 "/assets/player/color/"+playerData.getImageDirectory()+"/Teddy.png", DrawLayer.Entity);
-        teddy.setWidth(70);
-        teddy.setHeight(150);
+        teddy.setSize(teddyImgSize.x,teddyImgSize.y);
         teddy.addToScreen(this,true);
     }
 
     private void initButtons() {
         ArrayList<Clickable> butts = new ArrayList<>();
         //Main Menu Button
-        Button mainMenuButton = new gameobject.renderable.button.Button(765,30,
+        Button button = new gameobject.renderable.button.Button(btnSpot.x,btnSpot.y,
                 "/assets/buttons/Button-MainMenu.png",
                 "/assets/buttons/Button-MainMenuPressed.png",
                 DrawLayer.Entity,
@@ -236,12 +252,12 @@ public class PauseMenu extends Overlay {
                     Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Main Menu");
                     screenManager.addScreen(new MainMenuScreen(screenManager));
                 });
-        mainMenuButton.setWidth(192);
-        mainMenuButton.setHeight(72);
-        mainMenuButton.addToScreen(this,true);
+        button.setSize(btnSize.x, btnSize.y);
+        button.addToScreen(this,true);
+        butts.add(button);
 
         //Save Button
-        Button saveButton = new gameobject.renderable.button.Button(765,132,
+        button = new gameobject.renderable.button.Button(btnSpot.x,btnSpot.y+(btnSize.y+btnSpot.y),
                 "/assets/buttons/Button-Save.png",
                 "/assets/buttons/Button-SavePressed.png",
                 DrawLayer.Entity,
@@ -249,12 +265,12 @@ public class PauseMenu extends Overlay {
                     //TODO Save stuff
                     Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Save");
                 });
-        saveButton.setWidth(192);
-        saveButton.setHeight(72);
-        saveButton.addToScreen(this,true);
+        button.setSize(btnSize.x, btnSize.y);
+        button.addToScreen(this,true);
+        butts.add(button);
 
         //Options Button
-        Button optionsButton = new gameobject.renderable.button.Button(765,234,
+        button = new gameobject.renderable.button.Button(btnSpot.x,btnSpot.y+((btnSize.y+btnSpot.y) * 2),
                 "/assets/buttons/Button-Options.png",
                 "/assets/buttons/Button-OptionsPressed.png",
                 DrawLayer.Entity,
@@ -262,12 +278,12 @@ public class PauseMenu extends Overlay {
                     Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Options");
                     screenManager.addScreen(new OptionScreen(screenManager));
                 });
-        optionsButton.setWidth(192);
-        optionsButton.setHeight(72);
-        optionsButton.addToScreen(this,true);
+        button.setSize(btnSize.x, btnSize.y);
+        button.addToScreen(this,true);
+        butts.add(button);
 
         //Back Button
-        Button backButton = new gameobject.renderable.button.Button(765,336,
+        button = new gameobject.renderable.button.Button(btnSpot.x,btnSpot.y+((btnSize.y+btnSpot.y) * 3),
                 "/assets/buttons/Button-Back.png",
                 "/assets/buttons/Button-BackPressed.png",
                 DrawLayer.Entity,
@@ -275,14 +291,9 @@ public class PauseMenu extends Overlay {
                     Debug.success(DebugEnabler.BUTTON_LOG,"Clicked Button - Back");
                     currentState = ScreenState.TransitionOff;
                 });
-        backButton.setWidth(192);
-        backButton.setHeight(72);
-        backButton.addToScreen(this,true);
-
-        butts.add(mainMenuButton);
-        butts.add(saveButton);
-        butts.add(optionsButton);
-        butts.add(backButton);
+        button.setSize(btnSize.x, btnSize.y);
+        button.addToScreen(this,true);
+        butts.add(button);
 
         setKeyHandler(new ClickableKeyHandler(butts));
     }
@@ -291,7 +302,7 @@ public class PauseMenu extends Overlay {
         //Set up the grid for the player inventory
         int rows = 7;
         int columns = 4;
-        playerGrid = new RenderableGridContainer(rows, columns, 50, 50, 15, 140);
+        playerGrid = new RenderableGridContainer(rows, columns, itemBtnSize, itemBtnSize, itemBtnSpot.x, itemBtnSpot.y);
 
         //region Add button to the Grid Containers
         int count = playerInventory.size();
@@ -299,6 +310,7 @@ public class PauseMenu extends Overlay {
         for (int i = 0; i < rows; i++){
             for (int j = 0; j < columns; j++){
                 ItemButton itemContainerButton = new ItemButton();
+                itemContainerButton.setSize(itemBtnSize,itemBtnSize);
                 playerGrid.addAt(itemContainerButton, i, j);
                 if (k < count) {
                     itemContainerButton.setItem(playerInventory.get(k));
@@ -314,54 +326,56 @@ public class PauseMenu extends Overlay {
 
     private void initEquipButtons(){
         //Create grid for the cross pattern equipment stuff
-        RenderableGridContainer equipGrid = new RenderableGridContainer(4, 3, 50, 50, 250, 140);
+        RenderableGridContainer equipGrid = new RenderableGridContainer(4, 3, itemBtnSize, itemBtnSize, eqpLblSpot.x+25, itemBtnSpot.y);
         //Equipment Buttons
+        //Helmet
         ItemButton equipHead =  new ItemButton();
+        equipHead.setSize(itemBtnSize,itemBtnSize);
         if(currentEquipment.get(Equipments.Helmet.ordinal()) != null) {
             equipHead.setItem(currentEquipment.get(Equipments.Helmet.ordinal()));
         }
-        //equipHead.setSlot(Equipments.Helmet.ordinal());//todo remove??
         equipGrid.addAt(equipHead, 0, 1);
         equipButtons.add(equipHead);
         setClickEvent(equipHead, itemDetails);
-
+        //OffHand
         ItemButton equipOffHand =  new ItemButton();
+        equipOffHand.setSize(itemBtnSize,itemBtnSize);
         if(currentEquipment.get(Equipments.OffHand.ordinal()) != null) {
             equipOffHand.setItem(currentEquipment.get(Equipments.OffHand.ordinal()));
         }
-        //equipHead.setSlot(Equipments.OffHand.ordinal());
         equipGrid.addAt(equipOffHand, 1, 0);
         equipButtons.add(equipOffHand);
         setClickEvent(equipOffHand, itemDetails);
-
+        //Chest
         ItemButton equipChest =  new ItemButton();
+        equipChest.setSize(itemBtnSize,itemBtnSize);
         if(currentEquipment.get(Equipments.Chest.ordinal()) != null) {
             equipChest.setItem(currentEquipment.get(Equipments.Chest.ordinal()));
         }
-        //equipHead.setSlot(Equipments.Chest.ordinal());
         equipGrid.addAt(equipChest, 1, 1);
         equipButtons.add(equipChest);
         setClickEvent(equipChest, itemDetails);
-
+        //Weapon
         ItemButton equipWeapon =  new ItemButton();
+        equipWeapon.setSize(itemBtnSize,itemBtnSize);
         if(currentEquipment.get(Equipments.Weapon.ordinal()) != null) {
             equipWeapon.setItem(currentEquipment.get(Equipments.Weapon.ordinal()));
         }
-        //equipHead.setSlot(Equipments.Weapon.ordinal());
         equipGrid.addAt(equipWeapon, 1, 2);
         equipButtons.add(equipWeapon);
         setClickEvent(equipWeapon, itemDetails);
-
+        //Legs
         ItemButton equipLegs =  new ItemButton();
+        equipLegs.setSize(itemBtnSize,itemBtnSize);
         if(currentEquipment.get(Equipments.Legs.ordinal()) != null) {
             equipLegs.setItem(currentEquipment.get(Equipments.Legs.ordinal()));
         }
-        //equipHead.setSlot(Equipments.Legs.ordinal());
         equipGrid.addAt(equipLegs, 2, 1);
         equipButtons.add(equipLegs);
         setClickEvent(equipLegs, itemDetails);
-
+        //Feet
         ItemButton equipFeet =  new ItemButton();
+        equipFeet.setSize(itemBtnSize,itemBtnSize);
         if(currentEquipment.get(Equipments.Feet.ordinal()) != null) {
             equipFeet.setItem(currentEquipment.get(Equipments.Feet.ordinal()));
         }
@@ -376,21 +390,21 @@ public class PauseMenu extends Overlay {
 
     private void initItemDisplay() {
         //Item details text box
-        itemDetails = new TextBox(460, 130, 300, 260, "",
-                new Font("NoScary", Font.PLAIN, 40), Color.WHITE);
+        itemDetails = new TextBox(itemTxtSpot.x, itemTxtSpot.y, 700, 500, "",
+                new Font("NoScary", Font.PLAIN, 60), Color.WHITE);
         itemDetails.addToScreen(this,true);
 
+        //Use Buttons
+        Font noScaryHeader2 = new Font("NoScary", Font.PLAIN, 70);
+        useButton = new ButtonText(btnUseSpot.x, btnUseSpot.y, "/assets/buttons/Button-Inventory-Empty.png","/assets/buttons/Button-Inventory-Empty.png", DrawLayer.Entity,noScaryHeader2,Color.BLACK,btnUseSize.x,btnUseSize.y);
+        useButton.addToScreen(this, true);
+
         //Setup the big equipment button
-        bigEquipment =  new ItemButton(515,350, DrawLayer.Entity);
-        bigEquipment.setWidth(100);
-        bigEquipment.setHeight(100);
+        bigEquipment =  new ItemButton(btnBigSpot.x,btnBigSpot.y, DrawLayer.Entity);
+        bigEquipment.setSize(itemBigSize,itemBigSize);
+
         //If you click the big view do nothing, can change this later
         bigEquipment.setOnClick( () -> bigEquipment.deSelect());
         bigEquipment.addToScreen(this, true);
-
-        //Use Buttons
-        Font noScaryHeader2 = new Font("NoScary", Font.PLAIN, 35);
-        useButton = new ButtonText(503, 465, "/assets/buttons/Button-Inventory-Empty.png","/assets/buttons/Button-Inventory-Empty.png", DrawLayer.Entity,noScaryHeader2,Color.BLACK);
-        useButton.addToScreen(this, true);
     }
 }
