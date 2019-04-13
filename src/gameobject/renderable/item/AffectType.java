3e105960c078f2eeb9f2e39ade56278cb6c7d4e1
Gina Quinn <mdwgina@gmail.com>
2019-04-12 19:28:05 -0500

    I made cookies

--- src/gameobject/renderable/item/AffectType.java
+++ src/gameobject/renderable/item/AffectType.java
@@ -2,9 +2,7 @@ package gameobject.renderable.item;
 
 public enum AffectType {
     fire,
-    puncture,
     healthBoost,    // immediate increase/decrease
     healthLevel,     // increase/decrease max possible health
     enchant,
-    random
 }
